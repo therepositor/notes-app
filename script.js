6d20ffc5b46cf6407f99bb46fce4d293bbc52b27
Joseph <66221058+therepositor@users.noreply.github.com>
2020-12-01 15:14:44 +0100

    Add files via upload
    - Add instant note from just a click.
    -Delete any note saved by just a click.

---
+++ script.js
@@ -0,0 +1,71 @@
+ //SELECTORS
+const addBtn = document.getElementById("add");
+
+const notes = JSON.parse(localStorage.getItem("notes"));
+ // EVENT LISTENERS
+ addBtn.addEventListener("click", () => {
+     addNewNote();
+ });
+ if(notes) {
+     notes.forEach(note =>  {
+         addNewNote(note);
+     })
+ }
+ //FUNCTION
+ function addNewNote(text = "") {
+     const note = document.createElement("div");
+     note.classList.add("note");
+
+     note.innerHTML = `
+     <div class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <div class="main ${text ? "" : "hidden"}">
+        </div>
+        <textarea class="${text ? "hidden" : ""}"></textarea>   
+     </div> 
+     `;
+     //  const notesEl = note.querySelector(".notes");
+        const editBtn = note.querySelector(".edit");
+        const deleteBtn = note.querySelector(".delete");
+        const main = note.querySelector(".main");
+        const textArea = note.querySelector("textarea");
+        textArea.value = text;
+        main.innerHTML = marked(text);
+     deleteBtn.addEventListener("click", () =>   {
+     note.remove();
+
+     updateLS();
+      });
+
+     editBtn.addEventListener("click", () => {
+     main.classList.toggle("hidden");
+     textArea.classList.toggle("hidden");
+      })
+     // text input
+     textArea.addEventListener("input", (e) =>   {
+     const { value } = e.target;
+     console.log({ value });
+     main.innerHTML = marked(value);
+     console.log(main.innerHTML);
+     
+
+     updateLS();
+      });
+
+      document.body.appendChild(note);
+}
+     
+function updateLS() {
+    const notesText = document.querySelectorAll("textarea");
+    const notes = [];
+
+    notesText.forEach(note => {
+        notes.push(note.value);
+    })
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
+     
